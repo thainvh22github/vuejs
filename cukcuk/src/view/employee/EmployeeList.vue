6054dbf7afe7011dcc4b116e3662f62ace8f8d82
thainvh22github <thainvh22github@gmail.com>
2022-08-27 08:18:49 +0700

    feat: Them su kien cho btn add

--- cukcuk/src/view/employee/EmployeeList.vue
+++ cukcuk/src/view/employee/EmployeeList.vue
@@ -3,7 +3,7 @@
         <div class="m-table">
             <div class="m-header-table">
                 <div class="m-title-table">Danh sách nhân viên</div>
-                <button id="btn-add" class="btn btn-icon m-icon-add btn-add">Thêm nhân viên</button>
+                <button id="btn-add" class="btn btn-icon m-icon-add btn-add" @click = "btnOpenFormAddOnclick">Thêm nhân viên</button>
             </div>
             <div class="m-toolbar-table">
                 <div class="m-sort">
@@ -31,30 +31,30 @@
                 <table>
                     <thead>
                         <tr>
-                            <td class="text-left">Mã nhân viên</td>
-                            <td class="text-left">Họ và tên</td>
-                            <td class="text-left">Giới tính</td>
-                            <td class="text-center">Ngày sinh</td>
-                            <td class="text-left">Điện thoại</td>
-                            <td class="text-left">Email</td>
-                            <td class="text-left">Chức vụ</td>
-                            <td class="text-left">Phòng ban</td>
-                            <td class="text-rigth">Mức lương cơ bản</td>
-                            <td class="text-left">Tình trạng công việc</td>
+                            <th class="text-left">Mã nhân viên</th>
+                            <th class="text-left">Họ và tên</th>
+                            <th class="text-left">Giới tính</th>
+                            <th class="text-center">Ngày sinh</th>
+                            <th class="text-left">Điện thoại</th>
+                            <th class="text-left">Email</th>
+                            <th class="text-left">Chức vụ</th>
+                            <th class="text-left">Phòng ban</th>
+                            <th class="text-rigth">Mức lương cơ bản</th>
+                            <th class="text-left">Tình trạng công việc</th>
                         </tr>
                     </thead>
                     <tbody>
-                        <tr v-for="employee in employees" :key="employee.departmentsID">
-                            <td class="text-left">{{employee.departmentsCode}}</td>
-                            <td class="text-left">{{employee.departmentsName}}</td>
-                            <td class="text-left">Nam</td>
-                            <td class="text-center">22/10/20000</td>
-                            <td class="text-left">0868886703</td>
-                            <td class="text-left">thainvh22@gmail.com</td>
-                            <td class="text-left">Fresher</td>
-                            <td class="text-left">Viện</td>
-                            <td class="text-rigth">3.000.000</td>
-                            <td class="text-left">Đang đi làm</td>
+                        <tr v-for="(employee) in employees" :key="employee.employeesID">
+                            <td class="text-left">{{employee.employeesCode}}</td>
+                            <td class="text-left">{{employee.employeesName}}</td>
+                            <td class="text-left">{{employee.gender}}</td>
+                            <td class="text-center">{{employee.dateOfBirth}}</td>
+                            <td class="text-left">{{employee.phoneNumber}}</td>
+                            <td class="text-left">{{employee.email}}</td>
+                            <td class="text-left">{{employee.positionsID}}</td>
+                            <td class="text-left">{{employee.departmentsID}}</td>
+                            <td class="text-rigth">{{employee.salary}}</td>
+                            <td class="text-left">{{employee.workStatus}}</td>
                         </tr>
                     </tbody>
                 </table>
@@ -77,18 +77,24 @@
             <div class="m-employee-paging">10 nhân viên trên trang</div>
         </div>
     </div>
-
-    <EmployeeDetail />
+    <!-- @isShowDialog="showEmployeeDetail định nghĩa để dùng hàm emit -->
+    
+    <EmployeeDetail :isShow ="isShowDialog" @isShowDialog="showEmployeeDetail"/>
+    
+    
 </template>
 
 <script>
 import axios from 'axios'
-import EmployeeDetail from './EmployeeDetail.vue'
+import EmployeeDetail from '@/view/employee/EmployeeDetail.vue'
+
     export default {
-    name: "EmployeeList",
-    component: {
+    name:"EmployeeList",
+    components: {
         EmployeeDetail
     },
+    
+    
     /**giai đoạn 1: beforeCreate ( setup() )
      * không thể truy cập vào data, Dom
      */
@@ -102,14 +108,14 @@ import EmployeeDetail from './EmployeeDetail.vue'
         dùng để load dữ liệu cho sớm
      */
     created() {
-        let me = this;
+        var me = this;
         console.log("created");
         // gọi api để lấy dữ liệu sử dụng axios
         axios
-            .get("https://localhost:7285/api/Departments")
+            .get("https://localhost:7285/api/v1/Employees?pageSize=8&pageNumber=1")
             .then((response) => {
-            console.log(response.data);
-            me.employees = response.data;
+            console.log(response.data.data);
+            me.employees = response.data.data;
         });
     },
     /**giai đoạn 3: beforeMount
@@ -135,38 +141,64 @@ import EmployeeDetail from './EmployeeDetail.vue'
        lấy hết data thì event.curentTarget.value
      */
     beforeUpdate() {
+        console.log("beforeUpdate");
+    
     },
     /**giai đoạn 6: updated
      * truy cập vào data, element
        truy cập vào trong dom
      */
     updated() {
+        console.log("updated");
+
     },
     /**giai đoạn 7: beforeUnmount
      * truy cập vào data, element
        truy cập vào trong dom
      */
     beforeUnmount() {
+        console.log("beforeUnmount");
+
     },
     unmounted() {
+        console.log("unmounted");
+
     },
+
+    
+
     methods: {
-        btnAddOnClick() {
-            return "add on click";
+
+        /**
+         * Author: NVHThai (26/08/2022)
+         * Thêm mới nhân viên
+         */
+        btnOpenFormAddOnclick(){
+            this.showEmployeeDetail(true);
         },
-        getName() {
-            return this.fullName;
-        }
+
+
+        /**
+         * Author: NVHThai (26/08/2022)
+         * @param {boolean} isShow: true là hiển thị, flase là ẩn form 
+         */
+        showEmployeeDetail(isShow){
+            this.isShowDialog = isShow;
+            console.log("a"+isShow);
+        },
     },
-    formatDate() {
-    },
+
+
+    // formatDate() {
+    // },
+    
+    
     data() {
         return {
-            fullName: "Nguyen Vu Hai Thai",
-            employees: null
+            isShowDialog : false
         };
     },
-    components: { EmployeeDetail }
+    
 }
 </script>
 
